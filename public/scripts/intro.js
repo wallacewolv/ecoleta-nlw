3afa20ee393177a278ffbd2dfb5d0c630497c6a1
unknown <wallace_wolv@hotmail.com>
2020-07-30 10:37:26 -0300

    Projeto finalizado

---
+++ public/scripts/intro.js
@@ -0,0 +1,69 @@
+// comments
+// document.write("Hello")
+
+// variaveis, tipos de dados
+// let myvar = "He"
+// const myconst = "He"
+
+// document.write(myconst + myvar)
+
+// string
+// "Isso é uma string"
+// 'Issso tambem é uma string'
+// `Isso é uma string tambem`
+
+// number
+// const n1 = 1
+// const n2 = 2
+// document.write(n1 + n2)
+
+// boolen - true ou false
+// const bTrue = true
+// const bFalse = false
+// document.write(bFlase)
+
+// objeto possuem propriedades e funcionalidades
+// const pessoa = {
+// altura: "1,80m",
+// idade: 24,
+// solteiro: true,
+// correr(){
+//    document.write("executar uma grande dogica de correr")
+//  }    
+// }
+
+// pesssoa.correr()
+
+// Array - Vetores
+// const colecaoDeBolinhas = ["bleu", "green", 1, {name:"My name"}]
+
+// document.write(colecaoDebolinhas[3].name)
+
+
+// Funções - Funcionalidades - Atalhos- reuso de código
+
+// // resgistrar função
+// function sayMyName(name){
+
+//   document.write(name)   
+// }
+
+// // executar
+// sayMyName("Douglas")
+// sayMyName("Valeska")
+// sayMyName("Kyam")
+
+// condicionais
+
+// const notaFinal = 7
+
+// if( notaFinal < 5 ) {
+//   document.write("Reprovado") 
+// } else {
+//   document.write("Aprovado")   
+// }
+
+// loop repetições
+// for (i= 0; i < 10; i++) {
+//  domcument.write(`<p>${i}</p>`)
+// }
---
+++ src/server.js
@@ -0,0 +1,113 @@
+const express = require("express")
+const server = express()
+
+//pegar o banco de dados
+const db = require("./database/db.js")
+
+//configurar pasta publica
+server.use(express.static("public"))
+
+// habilitar o uso do req.body na nossa aplicação
+server.use(express.urlencoded({ extended: true}))
+
+// utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+  express: server,
+  noCache: true
+})
+
+
+
+// configurar caminhos da minha aplicação
+// página inicial
+// req: Requisição
+// res: Resposta
+server.get("/", (req,res) => {
+    return res.render("index.html", { title: "Um título"})
+})
+
+
+server.get("/create-point", (req,res) => {
+  //req.query: Query Strings da nossa url  
+  //console.log (req.query)
+  
+    return res.render("create-point.html")
+})
+
+server.post("/savepoint", (req, res) => {
+
+  //req.body: o corpo do nosso formulário
+  //console.log(req.body)
+
+  //inserir ados no banco de dados
+  const query = `
+    INSERT INTO places (
+      image,
+      name,
+      address,
+      address2,
+      state,
+      city,
+      items
+    ) VALUES (?,?,?,?,?,?,?);
+  `
+  const values = [
+    req.body.image,
+    req.body.name,
+    req.body.address,
+    req.body.address2,
+    req.body.state,
+    req.body.city,
+    req.body.items
+  ]
+
+  function afterInsertData(err) {
+    if(err) {
+      console.log(err)
+      return res.send("Erro no cadastro!")
+    }
+    console.log("Cadastrado com sucesso")
+    console.log(this)
+
+    return res.render("create-point.html", {saved: true})
+  }
+
+  db.run(query, values, afterInsertData)
+
+  
+})
+
+
+
+server.get("/search", (req,res) => {
+
+  const search =req.query.search
+
+  if(search == "") {
+    // Pesquisa vazia
+    return res.render("search-results.html", {total: 0})
+  }
+  
+  //Pegar os dados do banco de dados
+  // Quando colocamos WHERE city = '${search}', isso só funciona 
+  // com a pesquisa exata do nome da cidade. 
+  // Quando colocamos WHERE city LIKE '%${search}%', isso funciona
+  // colocando qualquer letra ou palavra relacionada a cidade pesquisada
+  db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows){
+    if(err) {
+      return console.log(err)
+    }
+    // O console.log serve pra consultar no terminal se deu certo
+    console.log("Aqui estão os seus registros:")
+    console.log(rows)
+
+    const total = rows.length
+    // Mostrar a página html com os dados do banco de dados
+    return res.render("search-results.html", {places: rows, total: total})
+  })
+
+})
+
+// ligando o servidor no localhost:3000
+server.listen(3000)
